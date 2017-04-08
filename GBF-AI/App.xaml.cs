45dbce53954faa535d23748d19df702f2be6136d
Chewy <Chewbakaaaa@live.com>
2017-04-08 04:46:46 -0400

    initial solution

---
+++ GBF-AI/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Gecko;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GBF_AI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Xpcom.EnableProfileMonitoring = false;
+            Xpcom.Initialize("Firefox");
+
+            GeckoPreferences.User["general.useragent.override"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36";
+        }
+    }
+}
